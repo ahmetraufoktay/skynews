2a9add0a9a3e2b43ec484b479a5a308271455d08
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-09-19 13:08:13 +0300

    [FEAT-2] Create nav Social media functionality soon to be added

---
+++ src/components/navinfo.jsx
@@ -0,0 +1,28 @@
+export default function NavInfo() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <div>
+          <h3 className="nav-headline">Technology News</h3>
+          <h4 className="nav-support-headline">
+            Your Number One News Source About Technology
+          </h4>
+        </div>
+        <div className="nav-social">
+          <button>
+            <img src="" alt="twitterpage" />
+          </button>
+          <button>
+            <img src="" alt="instagrampage" />
+          </button>
+          <button>
+            <img src="" alt="skylabpage" />
+          </button>
+          <button>
+            <img src="" alt="githubpage" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}

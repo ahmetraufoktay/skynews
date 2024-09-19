86cea552b6b19a2496ae0a4cab5633808b5d86ee
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-09-19 19:55:02 +0300

    [FEAT-12] Add handling multiple article pages

--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -1,7 +1,23 @@
 import useNewsData from "../useNewsData";
-
+import newsImage from "../assets/news.jpg";
+import { useState } from "react";
 export default function MainPage() {
   const { newsData, loading, error } = useNewsData();
+  const [startIndex, setStartIndex] = useState(0);
+
+  function handlePrevious() {
+    if (startIndex > 0) {
+      const newStart = startIndex - 10;
+      setStartIndex(newStart);
+    }
+  }
+
+  function handleNext() {
+    if (startIndex >= 0 && startIndex <= 90) {
+      const newStart = startIndex + 10;
+      setStartIndex(newStart);
+    }
+  }
 
   if (loading) {
     return <main>Loading...</main>;
@@ -10,23 +26,40 @@ export default function MainPage() {
   if (error) {
     return <main>Error fetching news data</main>;
   }
-  console.log(newsData);
   return (
     <main>
-      {newsData.articles.slice(0, 10).map((article, index) => (
-        <article className="news" key={index}>
-          {" "}
-          <img
-            className="news-images"
-            src={article.urlToImage}
-            alt="IMAGE NOT AVAILABLE"
-          />
-          <h2 className="news-headline">
-            <a href="#">{article.title}</a>
-          </h2>
-          <h3 className="news-description">{article.description}</h3>
-        </article>
-      ))}
+      {newsData.articles
+        .slice(startIndex, startIndex + 10)
+        .map((article, index) => (
+          <article className="news" key={index}>
+            {" "}
+            <img
+              className="news-images"
+              src={article.urlToImage ? article.urlToImage : newsImage}
+              alt="IMAGE NOT AVAILABLE"
+            />
+            <h2 className="news-headline">
+              <a href={article.url}>{article.title}</a>
+            </h2>
+            <h3 className="news-description">{article.description}</h3>
+          </article>
+        ))}
+      <div className="site-buttons-container">
+        <button
+          disabled={startIndex === 0}
+          className="site-buttons"
+          onClick={handlePrevious}
+        >
+          &lt; Previous
+        </button>
+        <button
+          disabled={90 <= startIndex}
+          className="site-buttons"
+          onClick={handleNext}
+        >
+          Next &gt;
+        </button>
+      </div>
     </main>
   );
 }

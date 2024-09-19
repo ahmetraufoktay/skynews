ee19bfb249401456b8c9039ddaf443178e80aa19
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-09-19 19:56:33 +0300

    [FEAT-13] Add search filtering

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,9 +1,13 @@
 import searchSvg from "../assets/search.svg";
 import filterSvg from "../assets/filter.svg";
+import useNewsData from "../useNewsData";
 import { useState } from "react";
-export default function Search() {
+
+export default function Search({ setResults }) {
   const [searchState, setSearchState] = useState(false);
   const [filterState, setFilterState] = useState(false);
+  const { newsData, loading, error } = useNewsData();
+  const [input, setInput] = useState("");
 
   const handleBlur = (e) => {
     if (!e.currentTarget.contains(e.relatedTarget)) {
@@ -11,6 +15,35 @@ export default function Search() {
     }
   };
 
+  function showResults(value, type) {
+    const results = newsData.articles.filter((article) => {
+      if (type == false) {
+        return (
+          value &&
+          article &&
+          article.title &&
+          article.title.toLowerCase().includes(value.toLowerCase())
+        );
+      } else if (type == true) {
+        return (
+          value &&
+          article &&
+          article.author &&
+          article.author.toLowerCase().includes(value.toLowerCase())
+        );
+      }
+    });
+    setResults(results);
+  }
+
+  function handleChange(value, type) {
+    setInput(value);
+    showResults(value, type);
+  }
+  function handleFilter() {
+    setInput("");
+    setFilterState(!filterState);
+  }
   return (
     <section className="searchbar" role="search" onBlur={handleBlur}>
       <button className="searchbutton">
@@ -20,11 +53,13 @@ export default function Search() {
         type="search"
         placeholder={`Search ${filterState ? "Author" : "Title"}`}
         className={`search ${filterState ? "author" : "title"}`}
+        value={input}
         onFocus={() => setSearchState(true)}
+        onChange={(e) => handleChange(e.target.value, filterState)}
       />
       <button
         className="filterbutton"
-        onClick={() => setFilterState(!filterState)}
+        onClick={() => handleFilter()}
         style={{
           opacity: searchState ? "1" : "0",
           visibility: searchState ? "visible" : "hidden",

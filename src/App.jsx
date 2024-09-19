8d37642825b7242d0de9e17aadd4ef88f144a363
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-09-19 15:40:27 +0300

    [FEAT-11] Add mainpage and aside to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Heading from "./components/heading";
 import NavInfo from "./components/navinfo";
+import MainPage from "./components/mainpage";
+import Aside from "./components/aside";
 function App() {
   return (
     <>
       <Heading></Heading>
       <NavInfo></NavInfo>
+      <div className="main-aside-tab" role="presentation">
+        <MainPage></MainPage>
+        <Aside></Aside>
+      </div>
     </>
   );
 }
